3c044ff7d4e3993afe048a6bd9686b0580dbb15a
w3raja <bigprotocol@gmail.com>
2019-04-30 20:10:17 +0530

    process.env.port added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const app = require('./app');
 
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 const server = http.createServer(app);
 
 
-server.listen(3000, function(){
+server.listen(PORT, function(){
 
     console.log("welcome to the web developemnt course with node.js"+PORT);
 })
